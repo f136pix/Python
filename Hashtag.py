38b44ce9ccb5c5444a670a6026fff9433892f816
Filipe Furlan <73194798+f136pix@users.noreply.github.com>
2022-05-27 19:13:52 -0300

    Add files via upload

---
+++ Hashtag.py
@@ -0,0 +1,16 @@
+#A partir de uma frase recebida em forma de string,a função ira formatar o texto retirando
+#os espaços, colocando e letras maiusculas após os espaços  e adicionando uma hashtag no começo.
+
+def hashtag(frase):
+    frase = frase.strip()
+    frase = frase.split(' ')
+    index = 0
+    while(index < len(frase)):
+        frase[index] = frase[index].capitalize()
+        
+        index= index +1
+    frase.insert(0,'#')
+    frase = ''.join(frase)
+    return(frase)
+
+
